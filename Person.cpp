e8415071ee0de9465c9ce5502bf1db2c719466e5
Florin Hasna <florinhasna@outlook.com>
2024-01-08 11:21:21 +0000

    Added methods for derived classes "Librarian" and "Member":  - Some methods are empty and commented out intentionally

--- Person.cpp
+++ Person.cpp
@@ -39,3 +39,76 @@ void Person::setEmail(std::string email)
 {
     this->email = email;
 }
+
+// LIBRARIAN DERIVED CLASS METHODS
+
+// constructor
+Librarian::Librarian(int staffID, std::string name, std::string address, std::string email, int salary) : Person(name, address, email)
+{
+    this->staffID = staffID;
+    this->salary = salary;
+}
+
+// void Librarian::addMember()
+// {   
+// }
+
+// void Librarian::issueBook(int memberID, int bookID)
+// {
+// }
+
+// void Librarian::returnBook(int memberID, int bookID)
+// {
+// }
+
+// void Librarian::displayBorrowedBooks(int memberID)
+// {
+// }
+
+// void Librarian::calcFine(int memberID)
+// {
+// }
+
+std::string Librarian::getStaffID()
+{
+    return std::to_string(staffID);
+}
+
+void Librarian::setStaffID(int staffID)
+{
+    this->staffID = staffID;
+}
+
+int Librarian::getSalary()
+{
+    return salary;
+}
+
+void Librarian::setSalary(int salary)
+{
+    this->salary = salary;
+}
+
+// MEMBER DERIVED CLASS METHODS 
+
+// constructor       
+Member::Member (int memberID, std::string name, std::string address, std::string email) : Person(name, address, email)
+{
+    this->memberID = memberID;
+}
+
+// returns the ID as a string
+int Member::getMemberId() 
+{
+      return memberID;
+}
+
+// // returns a vector of borrowed books
+// std::vector<Book> Member::getBooksBorrowed() 
+// {
+// }
+
+// // add a new book borrow
+// void Member::setBooksBorrowed(Book& book)
+// {
+// }
