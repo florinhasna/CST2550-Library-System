6e08ff7ba1cb4f69abdfe9d967c2bbfcc5479646
Florin Hasna <florinhasna@outlook.com>
2024-01-08 11:23:51 +0000

    Header file for class "Book"

---
+++ Book.h
@@ -0,0 +1,28 @@
+#ifndef _BOOK_H_
+#define _BOOK_H_
+#include <string>
+
+class Book
+{
+    private:
+        int bookID;
+        std::string bookName;
+        std::string authorFirstName;
+        std::string authorLastName;
+        std::string bookType;
+        Date dueDate;
+        Member& borrower();
+    public:
+        Book(int bookID, std::string bookName, std::string authorFirstName, std::string authorLastName);
+        virtual ~Book();
+        std::string getBookID();
+        std::string getBookName();
+        std::string getAuthorFirstName();
+        std::string getAuthorLastName();
+        Date getDueDate();
+        void setDueDate(Date dueDate);
+        void returnBook();
+        void borrowBook(Member& borrower, Date dueDate);
+};
+
+#endif
