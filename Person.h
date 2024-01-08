bb2d1f2a43454313c6aea54b997bbb51e7553dd8
Florin Hasna <florinhasna@outlook.com>
2024-01-08 10:51:41 +0000

    Created the header file for "Person" class

---
+++ Person.h
@@ -0,0 +1,21 @@
+#ifndef _PERSON_H_
+#define _PERSON_H_
+#include <string>
+
+class Person
+{
+    private:
+        std::string name;
+        std::string address;
+        std::string email;
+    public:
+        virtual ~Person();
+        std::string getName();
+        void setName(std::string name);
+        std::string getAddress();
+        void setAddress(std::string address);
+        std::string getEmail();
+        void setEmail(std::string email);
+};
+
+#endif
