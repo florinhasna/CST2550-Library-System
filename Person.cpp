6ee4bc146c7165034d21626c47324078282e6b78
Florin Hasna <florinhasna@outlook.com>
2024-01-12 02:44:52 +0000

    Completed class functionalitis, to be adjusted: 	  - calcFine has bugs, to be fixed 	  - to comment the code.
    Added input validation when a new member is created.

--- Person.cpp
+++ Person.cpp
@@ -3,7 +3,6 @@
 std::vector<Member> members;
 std::vector<Librarian> librarians;
 Date date_object;
-Date today_date;
 
 Person::~Person() // destructor
 { 
@@ -71,15 +70,19 @@ std::vector<Book*> Member::getBooksBorrowed()
 // add a new book borrow
 void Member::setBooksBorrowed(Book* book)
 {
+    if (!book) {
+        std::cout << "System error whilt trying to handle the request.\n"; // null pointer given
+        return;
+    }
+
     auto it = std::find(this->booksLoaned.begin(), this->booksLoaned.end(), book);
-    if(it != this->booksLoaned.end()){
-        this->booksLoaned.erase(it);
-        std::cout << book->getBookName() << " has been returned successfuly." << "\n";
+
+    if (it != this->booksLoaned.end()) {
+        it = this->booksLoaned.erase(it); // Erase the borrowed book and update the iterator
+        std::cout << book->getBookName() << " has been returned successfully.\n";
     } else {
         this->booksLoaned.push_back(book);
     }
-
-    std::cout << (int)this->booksLoaned.size();
 }
 
 // LIBRARIAN DERIVED CLASS METHODS
@@ -98,17 +101,33 @@ void Librarian::addMember()
 {   
     int mID; // memberID
     std::string name, address, email;
+    std::regex name_pattern("^[A-Za-z]{3,}\\s[A-Za-z]{3,}$");
+    std::regex address_pattern("^[1-9]+\\s[a-zA-Z]{3,}\\s[a-zA-Z]{3,}\\,\\s[A-Z0-9]{2,}\\s[A-Z0-9]{3,3}$");
+    std::regex email_pattern("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
 
-    // read in new member details
-    std::cout << "Enter a unique member ID: ";
-    std::cin >> mID;
+    // read in librarian details
+    do{
+        std::cout << "Enter a four numbers long unique staff ID: ";
+        std::cin >> mID;
+    } while (!(mID > 999 && mID < 10000));
+
     std::cin.ignore();
-    std::cout << "Enter name: ";
-    std::getline(std::cin, name);
-    std::cout << "Enter address: ";
-    std::getline(std::cin, address);
-    std::cout << "Enter E-Mail: ";
-    std::getline(std::cin, email);
+
+    do {
+        std::cout << "Enter full name: ";
+        std::getline(std::cin, name);
+    } while(!std::regex_match(name, name_pattern));
+    
+    do{
+        std::cout << "Enter first line of address and postcode in capitals separated by a coma...\n";
+        std::cout << "e.g.: 35 Hendon Way, NW9 7FJ -> Waiting input: ";
+        std::getline(std::cin, address);
+    } while (!std::regex_match(address, address_pattern));
+    
+    do {
+        std::cout << "Enter a valid E-Mail: ";
+        std::getline(std::cin, email);
+    } while (!std::regex_match(email, email_pattern));
 
     members.push_back(Member(mID, name, address, email));
 }
@@ -139,11 +158,12 @@ void Librarian::issueBook(int memberID, int bookID)
             if(flag){
                 try{
                     book->borrowBook(*borrower, date_object);
-                    borrower->setBooksBorrowed(book);
-                    std::cout << "\n" << "The book " << book->getBookName() << " was issued to " << borrower->getName() << "\n\n";
                 } catch (const std::exception& e) {
-                    std::cerr << "The specified book was borrowed by someone else. " << std::endl;
+                    break;
                 }
+                
+                borrower->setBooksBorrowed(book);
+                std::cout << "\n" << "The book " << book->getBookName() << " was issued to " << borrower->getName() << "\n\n";
                 break;
             }
         }
@@ -160,10 +180,10 @@ void Librarian::returnBook(int memberID, int bookID)
         if(members[i].getMemberId() == std::to_string(memberID)){
             std::vector<Book*> borrowed = members[i].getBooksBorrowed();
 
-            for(int i = 0; i < (int) borrowed.size(); i++){
-                if(borrowed[i]->getBookID() == std::to_string(bookID)){
-                    members[i].setBooksBorrowed(borrowed[i]);
-                    borrowed[i]->returnBook();
+            for(int j = 0; j < (int) borrowed.size(); j++){
+                if(borrowed[j]->getBookID() == std::to_string(bookID)){
+                    members[i].setBooksBorrowed(borrowed[j]);
+                    borrowed[j]->returnBook();
                 }
             }
             break;
@@ -200,16 +220,50 @@ void Librarian::displayBorrowedBooks(int memberID)
 
 void Librarian::calcFine(int memberID)
 {
+    Date today_date;
+    today_date.getCurrentDay();
     for(int i = 0; i < (int) members.size(); i++){
         if(members[i].getMemberId() == std::to_string(memberID)){
             std::vector<Book*> borrowed = members[i].getBooksBorrowed();
 
-            for(int i = 0; i < (int) borrowed.size(); i++){
-                int borrowal_day = borrowed[i]->getDueDate().day;
+            for(int j = 0; j < (int) borrowed.size(); j++){
+                int borrowal_day = borrowed[j]->getDueDate().day;
+                int borrowal_month = borrowed[j]->getDueDate().month;
 
-                if (borrowal_day > today_date.day){
-                    std::cout << borrowed[i]->getBookName() << " is " << borrowal_day - today_date.day << " days overdue." << std::endl;
-                    std::cout << "   - A fine of " << borrowal_day - today_date.day << "£ has to be paid." << std::endl;
+                std::cout << "Borrowal day " << borrowal_day << " " << borrowal_month << std::endl;
+                std::cout << "Curr day " << today_date.day << " " << today_date.month << std::endl;
+
+                if(borrowal_month < today_date.month){
+                    int days_in_month;
+                    int overdue = today_date.day - 3;
+
+                    for(int k = today_date.month - 1; k >= borrowal_month; k--){
+                        // months with 30 days
+                        if (k == 4 || k == 6 || k == 9 || k == 11){
+                            days_in_month = 30;  
+                        } else if(k == 2) { // february
+                            // checking if is a leap year or not
+                            if(today_date.year % 4 == 0){
+                                days_in_month = 29;
+                            } else {
+                                days_in_month = 28;
+                            }
+                        } else { // other months with 31
+                            days_in_month = 31;
+                        }
+
+                        overdue = overdue + days_in_month;
+                    }
+
+                    std::cout << borrowed[j]->getBookName() << " is " << overdue - borrowal_day << " days overdue." << std::endl;
+                    std::cout << "   - A fine of " << overdue - borrowal_day << "£ has to be paid." << std::endl;  
+                } else {
+                    if (today_date.day - borrowal_day > 3){
+                        std::cout << borrowed[j]->getBookName() << " is " << today_date.day - borrowal_day - 3 << " days overdue." << std::endl;
+                        std::cout << "   - A fine of " << today_date.day - borrowal_day - 3 << "£ has to be paid." << std::endl;
+                    } else {
+                        std::cout << "The member is not due any fine for the book " << borrowed[j]->getBookName() << "\n"; 
+                    }
                 }
             }
         }
--- Person.h
+++ Person.h
@@ -1,6 +1,5 @@
 #ifndef _PERSON_H_
 #define _PERSON_H_
-#include <iostream>
 #include "Book.h"
 
 // Base class
